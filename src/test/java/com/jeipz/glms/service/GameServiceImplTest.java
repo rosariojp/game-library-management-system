824ac555efe5beb26bbd69a8783d7dc86b6aa7a3
John Paul Rosario <rosario.jeipz@gmail.com>
2024-04-10 12:51:11 +0900

    Unit tests for GameServiceImpl

--- src/test/java/com/jeipz/glms/service/GameServiceImplTest.java
+++ src/test/java/com/jeipz/glms/service/GameServiceImplTest.java
@@ -1,8 +1,12 @@
 package com.jeipz.glms.service;
 
+import com.jeipz.glms.exception.GameAlreadyExistsException;
 import com.jeipz.glms.exception.GameNotFoundException;
 import com.jeipz.glms.mapper.GameMapper;
 import com.jeipz.glms.model.Game;
+import com.jeipz.glms.model.Genre;
+import com.jeipz.glms.model.Platform;
+import com.jeipz.glms.model.input.GameInput;
 import com.jeipz.glms.repository.GameRepository;
 import com.jeipz.glms.repository.GenreRepository;
 import com.jeipz.glms.repository.PlatformRepository;
@@ -15,6 +19,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -30,6 +36,8 @@ class GameServiceImplTest {
     private static final String GAME_TITLE_FIELD = "title";
 
     private static final String GAME_TITLE = "Dragon Quest XI S";
+
+    private static final String GAME_DESCRIPTION = "Dragon Quest Game";
 
     private static final Sort SORT = Sort.by(GAME_TITLE_FIELD).ascending();
 
@@ -61,6 +69,24 @@ class GameServiceImplTest {
                         .id(UUID.randomUUID())
                         .title("Game " + i)
                         .price(new BigDecimal("59.99"))
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private List<Genre> createGenreList() {
+        return IntStream.range(0, SIZE)
+                .mapToObj(i -> Genre.builder()
+                        .id(UUID.randomUUID())
+                        .name("Genre " + i)
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private List<Platform> createPlatformList() {
+        return IntStream.range(0, SIZE)
+                .mapToObj(i -> Platform.builder()
+                        .id(UUID.randomUUID())
+                        .name("Platform " + i)
                         .build())
                 .collect(Collectors.toList());
     }
@@ -117,6 +143,66 @@ class GameServiceImplTest {
         assertThrows(GameNotFoundException.class, () -> gameService.getGameById(id));
 
         verify(gameRepository, times(1)).findById(id);
+    }
+
+    @Test
+    public void addGame_withoutGenreAndPlatform_Successful() {
+        List<Genre> genres = createGenreList();
+        List<Platform> platforms = createPlatformList();
+        List<UUID> genreIds = genres.stream().map(Genre::getId).toList();
+        List<UUID> platformIds = platforms.stream().map(Platform::getId).toList();
+
+        GameInput gameInput = new GameInput(GAME_TITLE,
+                GAME_DESCRIPTION,
+                LocalDate.of(2024, 12, 30),
+                new BigDecimal("59.99"),
+                genreIds,
+                platformIds);
+
+        Game game = Game.builder()
+                .title(gameInput.title())
+                .description(gameInput.description())
+                .price(gameInput.price())
+                .releaseDate(gameInput.releaseDate())
+                .platforms(new HashSet<>(platforms))
+                .genres(new HashSet<>(genres))
+                .build();
+
+        doNothing().when(gameValidator).validateTitle(gameInput.title());
+        when(gameMapper.apply(gameInput)).thenReturn(game);
+        when(gameRepository.save(game)).thenReturn(game);
+
+        Game savedGame = gameService.addGame(gameInput);
+
+        assertAll("game should be equal to savedGame",
+                () -> assertEquals(game.getId(), savedGame.getId()),
+                () -> assertEquals(game.getTitle(), savedGame.getTitle()),
+                () -> assertEquals(game.getDescription(), savedGame.getDescription()),
+                () -> assertEquals(game.getReleaseDate(), savedGame.getReleaseDate()),
+                () -> assertEquals(game.getPrice(), savedGame.getPrice()));
+
+        verify(gameValidator, times(1)).validateTitle(gameInput.title());
+        verify(gameMapper, times(1)).apply(gameInput);
+        verify(gameRepository, times(1)).save(game);
+    }
+
+    @Test
+    public void addGame_AlreadyExists() {
+        GameInput gameInput = new GameInput(GAME_TITLE,
+                GAME_DESCRIPTION,
+                LocalDate.of(2024, 12, 30),
+                new BigDecimal("59.99"),
+                null,
+                null);
+
+        doThrow(GameAlreadyExistsException.class)
+                .when(gameValidator).validateTitle(gameInput.title());
+
+        assertThrows(GameAlreadyExistsException.class,
+                () -> gameService.addGame(gameInput));
+
+        verify(gameValidator, times(1))
+                .validateTitle(gameInput.title());
     }
 
 }
